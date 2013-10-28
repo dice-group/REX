8f3afec78f9d87bda07ddbb8d3627a90ddfaa4e9
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2013-10-28 12:11:40 +0100

    added Triples

--- src/main/java/org/aksw/rex/controller/RexController.java
+++ src/main/java/org/aksw/rex/controller/RexController.java
@@ -107,7 +107,7 @@ public class RexController {
 			log.error("XpathResults extracted: " + results.size());
 			// triple generation
 			quads = uriGenerator.getTriples(results, property);
-			BufferedWriter bw = new BufferedWriter(new FileWriter("ntFiles/goldstandard/withOutConsistency" + domain.toExternalForm().replaceAll("//", "").replaceAll("/", "") + ".txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter("ntFiles/withOutConsistency" + domain.toExternalForm().replaceAll("//", "").replaceAll("/", "") + ".txt"));
 			for (Quadruple<Node, Node, Node, String> q : quads) {
 				bw.write("<" + q.first.getURI() + "> <" + q.second().getURI() + "> <" + q.third().getURI() + "> <" + q.fourth() + ">.\n");
 			}
@@ -242,40 +242,15 @@ public class RexController {
 
 		ArrayList<ControllerData> d = new ArrayList<ControllerData>();
 
-		// new XPATHs from Disheng
-		// d.add(new ControllerData("imdb-title-index/",
-		// "http://dbpedia.org/ontology/starring", "http://www.imdb.com/title/",
-		// "//*[@id='titleCast']/TABLE/TBODY/TR/TD/A/SPAN/text()",
-		// "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]"));
-		// d.add(new ControllerData("imdb-name-index/",
-		// "http://dbpedia.org/ontology/starring", "http://www.imdb.com/name/",
-		// "//*[contains(text(),'Take The Quiz!')]/../SPAN[1]/A[1]/text()[1]",
-		// "//*[@id='filmo-head-actor' or @id='filmo-head-actress']/following-sibling::*[1]/DIV/B/A/text()"));
-		// d.add(new ControllerData("espnfc-player-index/",
-		// "http://dbpedia.org/ontology/team", "http://espnfc.com/player/_/id/",
-		// "//*[@class='profile']/H1/text()",
-		// "//*[contains(text(),'Teams')]/../UL/LI/A/text()"));
-		// d.add(new ControllerData("espnfc-team-index/",
-		// "http://dbpedia.org/ontology/team", "http://espnfc.com/team",
-		// "	//*[contains(text(),'SQUAD')]/../../DIV/DIV/TABLE/TBODY/TR[@class='evenrow' or @class='oddrow']/TD[3]/A/text()",
-		// "//*[@class='section-title']/text()"));
-		// d.add(new ControllerData("goodreads-author-index/",
-		// "http://dbpedia.org/ontology/author",
-		// "http://www.goodreads.com/author/",
-		// "//*[@class='authorName']/SPAN/text()",
-		// "//*[@class='bookTitle']/SPAN/text()"));
-		// d.add(new ControllerData("goodreads-book-index/",
-		// "http://dbpedia.org/ontology/author",
-		// "http://www.goodreads.com/book/",
-		// "//*[@id='bookAuthors']/SPAN[2]/A/SPAN/text()",
-		// "//*[@id='bookTitle']/text()"));
+		// ESOP XPATHs from Disheng
+		getESOPXpathData(d);
 
-		// old XPATHs from Disheng
-		getGoldenRuleData(d);
+		// golden XPATHs from Disheng
+//		 getGoldenRuleData(d);
 		for (ControllerData ds : d) {
 			try {
 				Property property = ResourceFactory.createProperty(ds.dbpediaProperty);
-				SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
+				SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpediaLiveOpenLink();
 
 				ExampleGenerator exampleGenerator = new SimpleExampleGenerator();
 				exampleGenerator.setMaxNrOfPositiveExamples(100);
@@ -304,7 +279,7 @@ public class RexController {
 				ConsistencyChecker c = new ConsistencyCheckerImpl(endpoint, namespace);
 
 				Set<Quadruple<Node, Node, Node, String>> quads = new RexController(property, exampleGenerator, domainIdentifier, xPathLearner, uriGenerator, c, endpoint).run(ds.subjectRule, ds.objectRule);
-				BufferedWriter bw = new BufferedWriter(new FileWriter("ntFiles/goldstandard/" + domain.toExternalForm().replaceAll("//", "").replaceAll("/", "") + ".txt"));
+				BufferedWriter bw = new BufferedWriter(new FileWriter("ntFiles/" + domain.toExternalForm().replaceAll("//", "").replaceAll("/", "") + ".txt"));
 				for (Quadruple<Node, Node, Node, String> q : quads) {
 					bw.write("<" + q.first.getURI() + "> <" + q.second().getURI() + "> <" + q.third().getURI() + "> <" + q.fourth() + ">.\n");
 				}
@@ -317,13 +292,26 @@ public class RexController {
 		}
 	}
 
+	private static void getESOPXpathData(ArrayList<ControllerData> d) {
+//		d.add(new ControllerData("imdb-title-index/", "http://dbpedia.org/ontology/director", "http://www.imdb.com/title/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[contains(text(),\"Director:\")]/../A[1]/SPAN[1]/TEXT()[1]"));
+
+//		d.add(new ControllerData("imdb-title-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/title/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[contains(text(),\"Stars:\")]/../A[1]/SPAN[1]/TEXT()[1]"));
+		d.add(new ControllerData("imdb-name-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/name/", "//*[contains(text(),\"Hide\")]/../../DIV[2]/DIV[1]/B[1]/A[1]/TEXT()[1]", "//SPAN[@itemprop='name'][1]/text()[1]"));
+//		d.add(new ControllerData("espnfc-player-index/", "http://dbpedia.org/ontology/team", "http://espnfc.com/player/_/id/", "//DIV[@class='profile']/H1[1]/text()[1]", "//TD[@align='left'][1]/text()[1]"));
+//		d.add(new ControllerData("espnfc-team-index/", "http://dbpedia.org/ontology/team", "http://espnfc.com/team", "//*[contains(text(),\"GOALS\")]/../../TR[2]/TD[1]/A[1]/TEXT()[1]", "/HTML/BODY[1]/DIV[2]/DIV[1]/DIV[1]/DIV[1]/DIV[4]/H1[1]/A[1]/text()[1]"));
+//		d.add(new ControllerData("goodreads-author-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/author/", "//A[@itemprop='url']/SPAN[1]/text()[1]", "//A[@class='authorName']/SPAN[1]/text()[1]"));
+//		d.add(new ControllerData("goodreads-book-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/book/", "//*[@id='bookTitle']/text()", "//*[contains(text(),\"api\")]/../../../../../../DIV[2]/DIV[1]/DIV[2]/DIV[3]/DIV[1]/DIV[2]/DIV[1]/SPAN[2]/A[1]/SPAN[1]/TEXT()[1]"));
+	}
+
 	private static void getGoldenRuleData(ArrayList<ControllerData> d) {
 
-		d.add(new ControllerData("imdb-title-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/title/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[@id='titleCast']/TABLE/TBODY/TR/TD/A/SPAN/text()"));
-		d.add(new ControllerData("imdb-name-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/name/","//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[@id='filmo-head-actor' or @id='filmo-head-actress']/following-sibling::*[1]/DIV/B/A/text()"));
+//		d.add(new ControllerData("imdb-title-index/", "http://dbpedia.org/ontology/director", "http://www.imdb.com/title/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[contains(text(),\"Director:\") or contains(text(),\"Directors:\")]/../A[1]/SPAN[1]/TEXT()[1]"));
+
+//		d.add(new ControllerData("imdb-title-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/title/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[@id='titleCast']/TABLE/TBODY/TR/TD/A/SPAN/text()"));
+//		d.add(new ControllerData("imdb-name-index/", "http://dbpedia.org/ontology/starring", "http://www.imdb.com/name/", "//*[contains(text(),\"Take The Quiz!\")]/../SPAN[1]/A[1]/TEXT()[1]", "//*[@id='filmo-head-actor' or @id='filmo-head-actress']/following-sibling::*[1]/DIV/B/A/text()"));
 		d.add(new ControllerData("espnfc-player-index/", "http://dbpedia.org/ontology/team", "http://espnfc.com/player/_/id/", "//*[@class='profile']/H1/text()", "//*[contains(text(),'Teams')]/../UL/LI/A/text()"));
-		d.add(new ControllerData("espnfc-team-index/", "http://dbpedia.org/ontology/team", "http://espnfc.com/team", "//*[contains(text(),'SQUAD')]/../../DIV/DIV/TABLE/TBODY/TR[@class='evenrow' or @class='oddrow']/TD[3]/A/text()", "//*[@class='section-title']/text()"));
-		d.add(new ControllerData("goodreads-author-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/author/", "//*[@class='bookTitle']/SPAN/text()", "//*[@class='authorName']/SPAN/text()"));
-		d.add(new ControllerData("goodreads-book-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/book/", "//*[@id='bookTitle']/text()", "//*[@id='bookAuthors']/SPAN[2]/A/SPAN/text()"));
+//		d.add(new ControllerData("espnfc-team-index/", "http://dbpedia.org/ontology/team", "http://espnfc.com/team", "//*[contains(text(),'SQUAD')]/../../DIV/DIV/TABLE/TBODY/TR[@class='evenrow' or @class='oddrow']/TD[3]/A/text()", "//*[@class='section-title']/text()"));
+//		d.add(new ControllerData("goodreads-author-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/author/", "//*[@class='bookTitle']/SPAN/text()", "//*[@class='authorName']/SPAN/text()"));
+//		d.add(new ControllerData("goodreads-book-index/", "http://dbpedia.org/ontology/author", "http://www.goodreads.com/book/", "//*[@id='bookTitle']/text()", "//*[@id='bookAuthors']/SPAN[2]/A/SPAN/text()"));
 	}
 }
