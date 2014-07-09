13b92aabb9a96b6e176f6a180e1801c8ea1aa5e2
l.wesemann <lars.wesemann@unister.de>
2014-07-09 17:03:50 +0200

    made teh RexController executable. changed the endpoint in the RexController to openLink virtuoso (maybe changed in the future)

--- src/main/java/org/aksw/rex/controller/RexController.java
+++ src/main/java/org/aksw/rex/controller/RexController.java
@@ -1,6 +1,12 @@
 package org.aksw.rex.controller;
 
+import java.net.URI;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -41,207 +47,227 @@ import edu.stanford.nlp.util.Quadruple;
  * @author ngonga
  */
 public class RexController {
-	Logger log = LoggerFactory.getLogger(RexController.class);
-	ExampleGenerator exampleGenerator;
-	DomainIdentifier di;
-	Property property;
-	XPathLearner xpath;
-	URIGenerator uriGenerator;
-	ConsistencyChecker consistency;
-	SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
-	int topNRules = 1;
-	private static URL domain;
+    private static Logger log = LoggerFactory.getLogger(RexController.class);
+    ExampleGenerator exampleGenerator;
+    DomainIdentifier di;
+    Property property;
+    XPathLearner xpath;
+    URIGenerator uriGenerator;
+    ConsistencyChecker consistency;
+    SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
+    int topNRules = 1;
+    private static URL domain;
 
-	public RexController(Property p, ExampleGenerator e, DomainIdentifier d, XPathLearner l, URIGenerator u, ConsistencyChecker c, SparqlEndpoint s) {
-		property = p;
-		exampleGenerator = e;
-		di = d;
-		xpath = l;
-		uriGenerator = u;
-		consistency = c;
-		endpoint = s;
-	}
+    public RexController(Property p, ExampleGenerator e, DomainIdentifier d, XPathLearner l, URIGenerator u,
+            ConsistencyChecker c, SparqlEndpoint s) {
+        property = p;
+        exampleGenerator = e;
+        di = d;
+        xpath = l;
+        uriGenerator = u;
+        consistency = c;
+        endpoint = s;
+    }
 
-	public static void main(String[] args) throws Exception {
-	  // Determine which property p you are looking for generating new triples <s p o>
+    public static void main(String[] args) throws Exception {
+        // Determine which property p you are looking for generating new triples <s p o>
         Property property = ResourceFactory.createProperty("http://dbpedia.org/ontology/director");
-        
-        		// Property property =
-		// ResourceFactory.createProperty("http://dbpedia.org/ontology/author");
-		
-        // The SPARQL endpoint provides examples as well as an underlying schema to validate generated triples
-        SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
 
+        // Property property =
+        // ResourceFactory.createProperty("http://dbpedia.org/ontology/author");
+
+        // The SPARQL endpoint provides examples as well as an underlying schema to validate generated triples
+        SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpediaLiveOpenLink();
+        log.info("ENDPOINT: {} ", endpoint.getURL());
         // The SimpleExampleGenerator retrieves 100 random triples from the underlying knowledge base with property p
         ExampleGenerator exampleGenerator = new SimpleExampleGenerator();
-        exampleGenerator.setMaxNrOfPositiveExamples(100);
+        exampleGenerator.setMaxNrOfPositiveExamples(1000);
         exampleGenerator.setEndpoint(endpoint);
         exampleGenerator.setPredicate(property);
 
-        // The ManualDomainIdentifier provides a starting point domain for the crawler. REX would also be capable of identifying a domain only by examples.
+        // The ManualDomainIdentifier provides a starting point domain for the crawler. REX would also be capable of
+        // identifying a domain only by examples.
         DomainIdentifier domainIdentifier = new ManualDomainIdentifier(new URL("http://www.imdb.com/title/"));
-		// DomainIdentifier domainIdentifier = new ManualDomainIdentifier(new
-		// URL("http://www.goodreads.com/author/"));
+        // DomainIdentifier domainIdentifier = new ManualDomainIdentifier(new
+        // URL("http://www.goodreads.com/author/"));
 
-        // The CrawlIndex is a LUCENE 4.X Index which will store the crawled information and provide it to the XPATHLearner.
+        // The CrawlIndex is a LUCENE 4.X Index which will store the crawled information and provide it to the
+        // XPATHLearner.
         CrawlIndex crawlIndex = new CrawlIndex("imdb-title-index/");
 
         // Our XPATHLearner learns a pair of XPATHs containing s,o for the given p
         XPathLearner xPathLearner = new ALFREDXPathLearner(crawlIndex);
         xPathLearner.setUseExactMatch(false);
-	// XPathExtractor xPathExtractor = new XPathExtractor(crawlIndex);
-		// XPathLearner xPathLearner = new XPathLearnerImpl(xPathExtractor,
-		// endpoint);
+        // XPathExtractor xPathExtractor = new XPathExtractor(crawlIndex);
+        // XPathLearner xPathLearner = new XPathLearnerImpl(xPathExtractor,
+        // endpoint);
 
-
-        // The URIGenerator is based on the AGDISTIS web service which will generate for a pair of s,o XPATH strings corresponding URIs from the knowledge base or new URIs if there are no to be found
+        // The URIGenerator is based on the AGDISTIS web service which will generate for a pair of s,o XPATH strings
+        // corresponding URIs from the knowledge base or new URIs if there are no to be found
         URIGenerator uriGenerator = new URIGeneratorAGDISTIS();
 
-        // After initialising the REX Controller with instances of components needed for our pipeline you can run the controller and see the results in the returned set of quadruples (provenance)
-        Set<Quadruple<Node, Node, Node, String>> quadruples = new RexController(property, exampleGenerator, domainIdentifier, xPathLearner, uriGenerator, new ConsistencyCheckerImpl(endpoint), endpoint).run();
-
-        for (Quadruple<Node, Node, Node, String> quadruple : quadruples) {
-            System.out.println(quadruple);
+        // After initialising the REX Controller with instances of components needed for our pipeline you can run the
+        // controller and see the results in the returned set of quadruples (provenance)
+        Set<Quadruple<Node, Node, Node, String>> quadruples = new RexController(property, exampleGenerator,
+                domainIdentifier, xPathLearner, uriGenerator, new ConsistencyCheckerImpl(endpoint), endpoint).run();
+        log.info("{} triples found", quadruples.size());
+        Path file = Paths.get("output.quad");
+        log.info("file: {}", file.toUri());
+        if (Files.exists(file)) {
+            Files.delete(file);
         }
-	
-	}
+        Files.createFile(file);
+        for (Quadruple<Node, Node, Node, String> quadruple : quadruples) {
+            String r = quadruple.toString() + "\n";
+            Files.write(file, r.getBytes(), StandardOpenOption.APPEND);
 
-	/**
-	 * Runs the extraction pipeline
-	 * 
-	 * @return A set of triples
-	 * @throws Exception
-	 *             If URI generation does not work
-	 */
-	public Set<Quadruple<Node, Node, Node, String>> run() throws Exception
+        }
 
-	{
-		Set<Quadruple<Node, Node, Node, String>> quads = Sets.newHashSet();
+    }
 
-		// example generation
-		Set<Pair<Resource, Resource>> posExamples = null;
-		Set<Pair<Resource, Resource>> negExamples = null;
-		posExamples = exampleGenerator.getPositiveExamples();
-		negExamples = exampleGenerator.getNegativeExamples();
+    /**
+     * Runs the extraction pipeline
+     * 
+     * @return A set of triples
+     * @throws Exception
+     *             If URI generation does not work
+     */
+    public Set<Quadruple<Node, Node, Node, String>> run() throws Exception
 
-		// domain identification
-		URL domain = di.getDomain(property, posExamples, negExamples, false);
+    {
+        Set<Quadruple<Node, Node, Node, String>> quads = Sets.newHashSet();
 
-		// XPath expression generation
-		List<Pair<XPathRule, XPathRule>> extractionRules = xpath.getXPathExpressions(posExamples, domain);
+        // example generation
+        Set<Pair<Resource, Resource>> posExamples = null;
+        Set<Pair<Resource, Resource>> negExamples = null;
+        log.info("get Positive Examples");
+        posExamples = exampleGenerator.getPositiveExamples();
+        log.info("get Positive Examples DONE: {}", posExamples.size());
+        log.info("get Negative Examples");
+        negExamples = exampleGenerator.getNegativeExamples();
+        log.info("get Negative Examples DONE: {}", negExamples.size());
+        // domain identification
+        log.info("get Domain");
+        URL domain = di.getDomain(property, posExamples, negExamples, false);
+        log.info("get Domain {}", domain.toString());
+        // XPath expression generation
+        log.info("get XPathExpressions");
+        List<Pair<XPathRule, XPathRule>> extractionRules = xpath.getXPathExpressions(posExamples, domain);
+        log.info("get XPathExpressions DONE");
 
-		if (!extractionRules.isEmpty()) {
-			// currently, we assume that the best rule is the first one in the
-			// list, thus we
-			extractionRules = extractionRules.subList(0, 1);
-			System.out.println("Top rule:\n" + extractionRules);
+        if (!extractionRules.isEmpty()) {
+            // currently, we assume that the best rule is the first one in the
+            // list, thus we
+            extractionRules = extractionRules.subList(0, 1);
+            System.out.println("Top rule:\n" + extractionRules);
 
-			// extract results from the corpus
-			Set<ExtractionResult> results = xpath.getExtractionResults(extractionRules, domain);
+            // extract results from the corpus
+            Set<ExtractionResult> results = xpath.getExtractionResults(extractionRules, domain);
 
-			// triple generation
-			quads = uriGenerator.getTriples(results, property);
+            // triple generation
+            quads = uriGenerator.getTriples(results, property);
 
-			// triple filtering
-			// triples = consistency.getConsistentTriples(triples,
-			// consistency.generateAxioms(endpoint));
-			Set<Triple> triples = quadsToTriples(quads);
-			triples = consistency.getConsistentTriples(triples);
-			quads = triplesToQuads(triples, quads);
-		}
+            // triple filtering
+            // triples = consistency.getConsistentTriples(triples,
+            // consistency.generateAxioms(endpoint));
+            Set<Triple> triples = quadsToTriples(quads);
+            triples = consistency.getConsistentTriples(triples);
+            quads = triplesToQuads(triples, quads);
+        }
 
-		return quads;
-	}
+        return quads;
+    }
 
-	/**
-	 * util function to convert triples to quadruples
-	 * 
-	 * @param triples
-	 * @param quads
-	 * @return
-	 */
-	private Set<Quadruple<Node, Node, Node, String>> triplesToQuads(Set<Triple> triples, Set<Quadruple<Node, Node, Node, String>> quads) {
-		HashSet<Quadruple<Node, Node, Node, String>> set = Sets.newHashSet();
-		for (Triple t : triples) {
-			for (Quadruple<Node, Node, Node, String> q : quads) {
-				if (t.getSubject().getURI().equals(q.first.getURI())) {
-					if (t.getPredicate().getURI().equals(q.second.getURI())) {
-						if (t.getObject().getURI().equals(q.third.getURI())) {
-							set.add(q);
-						}
-					}
-				}
-			}
-		}
-		return set;
-	}
+    /**
+     * util function to convert triples to quadruples
+     * 
+     * @param triples
+     * @param quads
+     * @return
+     */
+    private Set<Quadruple<Node, Node, Node, String>> triplesToQuads(Set<Triple> triples,
+            Set<Quadruple<Node, Node, Node, String>> quads) {
+        HashSet<Quadruple<Node, Node, Node, String>> set = Sets.newHashSet();
+        for (Triple t : triples) {
+            for (Quadruple<Node, Node, Node, String> q : quads) {
+                if (t.getSubject().getURI().equals(q.first.getURI())) {
+                    if (t.getPredicate().getURI().equals(q.second.getURI())) {
+                        if (t.getObject().getURI().equals(q.third.getURI())) {
+                            set.add(q);
+                        }
+                    }
+                }
+            }
+        }
+        return set;
+    }
 
-	/**
-	 * util function to convert quadruples to triples
-	 * 
-	 * @param quads
-	 * @return
-	 */
-	private Set<Triple> quadsToTriples(Set<Quadruple<Node, Node, Node, String>> quads) {
-		HashSet<Triple> set = Sets.newHashSet();
-		for (Quadruple<Node, Node, Node, String> q : quads) {
-			set.add(new Triple(q.first, q.second, q.third));
-		}
-		return set;
-	}
+    /**
+     * util function to convert quadruples to triples
+     * 
+     * @param quads
+     * @return
+     */
+    private Set<Triple> quadsToTriples(Set<Quadruple<Node, Node, Node, String>> quads) {
+        HashSet<Triple> set = Sets.newHashSet();
+        for (Quadruple<Node, Node, Node, String> q : quads) {
+            set.add(new Triple(q.first, q.second, q.third));
+        }
+        return set;
+    }
 
-	/**
-	 * Runs the extraction pipeline, used for gold standard evaluation
-	 * 
-	 * @param subjectRule
-	 * @param objectRule
-	 * 
-	 * @return A set of triples
-	 * @throws Exception
-	 *             If URI generation does not work
-	 */
-	public Set<Quadruple<Node, Node, Node, String>> run(String subjectRule, String objectRule) throws Exception
+    /**
+     * Runs the extraction pipeline, used for gold standard evaluation
+     * 
+     * @param subjectRule
+     * @param objectRule
+     * 
+     * @return A set of triples
+     * @throws Exception
+     *             If URI generation does not work
+     */
+    public Set<Quadruple<Node, Node, Node, String>> run(String subjectRule, String objectRule) throws Exception
 
-	{
-		Set<Quadruple<Node, Node, Node, String>> quads = Sets.newHashSet();
+    {
+        Set<Quadruple<Node, Node, Node, String>> quads = Sets.newHashSet();
 
-		// example generation
-		Set<Pair<Resource, Resource>> posExamples = null;
-		Set<Pair<Resource, Resource>> negExamples = null;
-		posExamples = exampleGenerator.getPositiveExamples();
-		negExamples = exampleGenerator.getNegativeExamples();
+        // example generation
+        Set<Pair<Resource, Resource>> posExamples = null;
+        Set<Pair<Resource, Resource>> negExamples = null;
+        posExamples = exampleGenerator.getPositiveExamples();
+        negExamples = exampleGenerator.getNegativeExamples();
 
-		// domain identification
-		domain = di.getDomain(property, posExamples, negExamples, false);
+        // domain identification
+        domain = di.getDomain(property, posExamples, negExamples, false);
 
-		// XPath expression generation
-		List<Pair<XPathRule, XPathRule>> extractionRules = new ArrayList<Pair<XPathRule, XPathRule>>();
+        // XPath expression generation
+        List<Pair<XPathRule, XPathRule>> extractionRules = new ArrayList<Pair<XPathRule, XPathRule>>();
 
-		extractionRules.add(new Pair<XPathRule, XPathRule>(new XPathRule(subjectRule), new XPathRule(objectRule)));
+        extractionRules.add(new Pair<XPathRule, XPathRule>(new XPathRule(subjectRule), new XPathRule(objectRule)));
 
-		if (!extractionRules.isEmpty()) {
-			extractionRules = extractionRules.subList(0, 1);
-			System.out.println("Top rule:\n" + extractionRules);
+        if (!extractionRules.isEmpty()) {
+            extractionRules = extractionRules.subList(0, 1);
+            System.out.println("Top rule:\n" + extractionRules);
 
-			// extract results from the corpus
-			Set<ExtractionResult> results = xpath.getExtractionResults(extractionRules, domain);
-			System.out.println("XpathResults extracted: " + results.size());
+            // extract results from the corpus
+            Set<ExtractionResult> results = xpath.getExtractionResults(extractionRules, domain);
+            System.out.println("XpathResults extracted: " + results.size());
 
-			// triple generation
-			quads = uriGenerator.getTriples(results, property);
-			System.out.println("Quadrupels generated extracted: " + quads.size());
+            // triple generation
+            quads = uriGenerator.getTriples(results, property);
+            System.out.println("Quadrupels generated extracted: " + quads.size());
 
-			// triple filtering
-			Set<Triple> triples = quadsToTriples(quads);
-			triples = consistency.getConsistentTriples(triples);
-			quads = triplesToQuads(triples, quads);
+            // triple filtering
+            Set<Triple> triples = quadsToTriples(quads);
+            triples = consistency.getConsistentTriples(triples);
+            quads = triplesToQuads(triples, quads);
 
-			// triples = consistency.getConsistentTriples(triples,
-			// consistency.generateAxioms(endpoint));
-			System.out.println("Consistency checked: " + quads.size());
+            // triples = consistency.getConsistentTriples(triples,
+            // consistency.generateAxioms(endpoint));
+            System.out.println("Consistency checked: " + quads.size());
 
-		}
+        }
 
-		return quads;
-	}
+        return quads;
+    }
 }
--- src/test/java/org/aksw/rex/consistency/ConsistencyCheckerImplTest.java
+++ src/test/java/org/aksw/rex/consistency/ConsistencyCheckerImplTest.java
@@ -5,12 +5,15 @@ package org.aksw.rex.consistency;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.dllearner.core.owl.Axiom;
 import org.dllearner.core.owl.ObjectProperty;
 import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,62 +22,74 @@ import com.hp.hpl.jena.graph.Triple;
 
 /**
  * @author Lorenz Buehmann
- *
+ * 
  */
 public class ConsistencyCheckerImplTest {
-	
-	SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
-//	SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpediaLOD2Cloud();
-	String namespace = "http://dbpedia.org/ontology/";
-	ConsistencyChecker consistencyChecker = new ConsistencyCheckerImpl(endpoint, namespace);
-	ObjectProperty property = new ObjectProperty("http://dbpedia.org/ontology/director");
-	static final String DBR = "http://dbpedia.org/resource/";
-	static final String DBO = "http://dbpedia.org/ontology/";
-	static final String REX = "http://aksw.org/resource/";
-	
-	@BeforeClass
-	public static void setup(){
-		
-	}
 
-	/**
-	 * Test method for {@link org.aksw.rex.consistency.ConsistencyCheckerImpl#getConsistentTriples(java.util.Set, java.util.Set)}.
-	 */
-	@Test
-	public void testGetConsistentTriples() {
-		Set<Triple> triples = new HashSet<Triple>();
-		Triple triple = Triple.create(
-				Node.createURI(DBR + "Die_Hard"), 
-				Node.createURI(property.getName()), 
-				Node.createURI(DBR + "Steven_Spielberg"));
-		triples.add(triple);
-		triple = Triple.create(
-				Node.createURI(DBR + "Leipzig"), 
-				Node.createURI(property.getName()), 
-				Node.createURI(DBR + "Steven_Spielberg"));
-		triples.add(triple);
-		triple = Triple.create(
-				Node.createURI(DBR + "The_Lord_of_the_Rings:_The_Fellowship_of_the_Ring"), 
-				Node.createURI(property.getName()), 
-				Node.createURI(REX + "Axel_Ngonga"));
-		triples.add(triple);
-		
-		Set<Triple> consistentTriples = consistencyChecker.getConsistentTriples(triples);
-		//first triple should be fine
-		//second triple should be omitted because
-		//* dbr:Leipzig is typed as dbo:Place
-		//* the generated domain of dbo:director is dbo:Work
-		//* a disjointness between dbo:Place and dbo:Work is detected based on the instances data
-		//third triple should be fine
-		assertTrue(consistentTriples.size() == 2);
-	}
+    // SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
+    // SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpediaLiveAKSW();
+    SparqlEndpoint endpoint;
+    String namespace = "http://dbpedia.org/ontology/";
+    ConsistencyChecker consistencyChecker;
+    ObjectProperty property = new ObjectProperty("http://dbpedia.org/ontology/director");
+    static final String DBR = "http://dbpedia.org/resource/";
+    static final String DBO = "http://dbpedia.org/ontology/";
+    static final String REX = "http://aksw.org/resource/";
 
-	/**
-	 * Test method for {@link org.aksw.rex.consistency.ConsistencyCheckerImpl#generateAxioms(org.dllearner.kb.sparql.SparqlEndpoint, org.dllearner.core.owl.ObjectProperty[])}.
-	 */
-	@Test
-	public void testGenerateAxiomsSparqlEndpointObjectPropertyArray() {
-		Set<Axiom> axioms = consistencyChecker.generateAxioms(endpoint, property);
-	}
+    @Before
+    public void setup() {
+        try {
+            endpoint = new SparqlEndpoint(new URL("http://live.dbpedia.org/sparql"));
+            
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        consistencyChecker = new ConsistencyCheckerImpl(endpoint, namespace);
+
+    }
+
+    /**
+     * Test method for
+     * {@link org.aksw.rex.consistency.ConsistencyCheckerImpl#getConsistentTriples(java.util.Set, java.util.Set)}.
+     */
+    @Test
+    public void testGetConsistentTriples() {
+        Set<Triple> triples = new HashSet<Triple>();
+        Triple triple = Triple.create(
+                Node.createURI(DBR + "Die_Hard"),
+                Node.createURI(property.getName()),
+                Node.createURI(DBR + "Steven_Spielberg"));
+        triples.add(triple);
+        triple = Triple.create(
+                Node.createURI(DBR + "Leipzig"),
+                Node.createURI(property.getName()),
+                Node.createURI(DBR + "Steven_Spielberg"));
+        triples.add(triple);
+        triple = Triple.create(
+                Node.createURI(DBR + "The_Lord_of_the_Rings:_The_Fellowship_of_the_Ring"),
+                Node.createURI(property.getName()),
+                Node.createURI(REX + "Axel_Ngonga"));
+        triples.add(triple);
+
+        Set<Triple> consistentTriples = consistencyChecker.getConsistentTriples(triples);
+        // first triple should be fine
+        // second triple should be omitted because
+        // * dbr:Leipzig is typed as dbo:Place
+        // * the generated domain of dbo:director is dbo:Work
+        // * a disjointness between dbo:Place and dbo:Work is detected based on the instances data
+        // third triple should be fine
+        assertTrue(consistentTriples.size() == 2);
+    }
+
+    /**
+     * Test method for
+     * {@link org.aksw.rex.consistency.ConsistencyCheckerImpl#generateAxioms(org.dllearner.kb.sparql.SparqlEndpoint, org.dllearner.core.owl.ObjectProperty[])}
+     * .
+     */
+    @Test
+    public void testGenerateAxiomsSparqlEndpointObjectPropertyArray() {
+        Set<Axiom> axioms = consistencyChecker.generateAxioms(endpoint, property);
+    }
 
 }
